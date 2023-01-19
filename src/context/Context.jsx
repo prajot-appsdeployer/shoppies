40aa40a8dd7369b1010bebf061f1fa39a8465b54
prajot-appsdeployer <prajot@appsdeployer.com>
2023-01-19 17:20:44 +0530

    fixed a issue where on the private routes when reloading it shows login page

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,10 +1,13 @@
 import { onAuthStateChanged } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { auth } from "../firebase";
 
 export const GlobalContext = createContext();
 
 export const Context = (props) => {
+  const navigate = useNavigate();
+
   // User Login state
   const [userState, setUserState] = useState(null);
 
@@ -12,6 +15,7 @@ export const Context = (props) => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setUserState(user);
+        window.history.back();
       } else {
         setUserState(null);
       }
